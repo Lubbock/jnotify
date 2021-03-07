5e900f6b46ae8182a1181a5374fd5b55fe74589a
ayer <Ayer1994@outlook.com>
2021-03-07 21:29:57 +0800

    feature: 增加项目注册代码
    1. 增加项目注册代码

--- src/main/java/com/lame/jnotify/Jnotify.java
+++ src/main/java/com/lame/jnotify/Jnotify.java
@@ -5,21 +5,17 @@ import com.lame.jnotify.notify.execute.ConsumerExecute;
 import com.lame.jnotify.notify.execute.ScheduledConsumerExecute;
 import com.lame.jnotify.notify.jobs.GitSyncJob;
 import com.lame.jnotify.notify.jobs.Job;
+import com.lame.jnotify.register.RepoRegister;
 import com.lame.jnotify.utils.JFileUtil;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.io.filefilter.HiddenFileFilter;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.monitor.FileAlterationMonitor;
 import org.apache.commons.io.monitor.FileAlterationObserver;
-import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Jnotify {
@@ -35,31 +31,22 @@ public class Jnotify {
         return observer;
     }
 
-    public static void registerPj() throws Exception{
-        String file = Object.class.getResource("/project").getFile();
-        List<String> lines = new ArrayList<>();
-        lines.add("d:/adbs,d:/adbscp");
-        FileUtils.writeLines(new File(file),"utf-8", lines, true);
-    }
 
     public static void main(String[] args) throws Exception{
-        registerPj();
-        String file = Object.class.getResource("/project").getFile();
-        final List<String> pjs = FileUtils.readLines(new File(file), Charset.forName("utf-8"));
+        RepoRegister repoRegister = new RepoRegister();
         List<FileAlterationObserver> observers = new ArrayList<>();
         List<Job> jobs = new ArrayList<>();
-        for (String pj : pjs) {
-            String[] split = pj.split(",");
-            System.out.println(String.format("检测到注册项目[%s]-[%s]\n准备第一次全文件夹同步", split[0], split[1]));
-            JFileUtil.copyTree(split[0], split[1], JFileUtil.PJ_DES_EXCLUDE);
-            FileAlterationObserver observer = jnotify(split[0], split[1]);
-            System.out.println(String.format("生成[%s]-[%s]文件夹同步配置", split[0], split[1]));
+        repoRegister.foreachSyncProject(((source, syncpkg) -> {
+            System.out.println(String.format("检测到注册项目[%s]-[%s]\n准备第一次全文件夹同步", source, syncpkg));
+            JFileUtil.copyTree(source, syncpkg, JFileUtil.PJ_DES_EXCLUDE);
+            FileAlterationObserver observer = jnotify(source, syncpkg);
+            System.out.println(String.format("生成[%s]-[%s]文件夹同步配置", source, syncpkg));
             observers.add(observer);
-            Job job = new GitSyncJob(split[1]);
+            Job job = new GitSyncJob(syncpkg);
             jobs.add(job);
-        }
+        }));
         long interval = TimeUnit.SECONDS.toMillis(1);
-        FileAlterationMonitor monitor = new FileAlterationMonitor(interval, observers.toArray(new FileAlterationObserver[observers.size()]));
+        FileAlterationMonitor monitor = new FileAlterationMonitor(interval, observers.toArray(new FileAlterationObserver[0]));
         monitor.start();
         ConsumerExecute.start();
         System.out.println("开启文件同步服务");
---
+++ src/main/java/com/lame/jnotify/register/RepoRegister.java
@@ -0,0 +1,60 @@
+package com.lame.jnotify.register;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class RepoRegister {
+
+    private String basePkg = "";
+
+    public void init() throws Exception {
+        Properties properties = new Properties();
+        properties.load(Object.class.getResourceAsStream("/jnotify.properties"));
+        basePkg = properties.getProperty("repo.base.package");
+        File file = new File(basePkg);
+        file.mkdirs();
+        for (String spj : getSyncProject()) {
+            String[] pjArray = spj.split(",");
+            File temp = new File(basePkg, pjArray[1]);
+            if (!temp.exists()) {
+                temp.mkdirs();
+            }
+        }
+    }
+
+    public void addNewProject(String f1) throws Exception {
+        File souce = new File(f1);
+        File syncdir = new File(basePkg, souce.getName() + "-" + System.currentTimeMillis());
+        String file = Object.class.getResource("/project").getFile();
+        List<String> lines = new ArrayList<>();
+        lines.add(souce.getAbsolutePath() + "," + syncdir.getAbsolutePath());
+        FileUtils.writeLines(new File(file), "utf-8", lines, true);
+    }
+
+    public List<String> getSyncProject() throws Exception {
+        String file = Object.class.getResource("/project").getFile();
+        List<String> pjs = FileUtils.readLines(new File(file), Charset.forName("utf-8"));
+        return pjs;
+    }
+
+    public interface RepoDes{
+        void accept(String source, String syncpkg) throws Exception;
+    }
+
+    public void foreachSyncProject(RepoDes repoDes)throws Exception {
+        final List<String> syncProject = getSyncProject();
+        syncProject.stream().forEach(s->{
+            String[] split = s.split(",");
+            try {
+                repoDes.accept(split[0], split[1]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
