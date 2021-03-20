2d6d97d417608176fe25f003f0906e50fc0ed914
ayer <Ayer1994@outlook.com>
2021-03-21 01:46:02 +0800

    fix: 增加windows 安装脚本
    增加windows安装脚本

--- jnotify-core/src/main/java/com/lame/jnotify/MultiJnotify.java
+++ jnotify-core/src/main/java/com/lame/jnotify/MultiJnotify.java
@@ -1,4 +1,23 @@
 package com.lame.jnotify;
 
+import com.lame.jnotify.notify.JnotifyCtx;
+
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class MultiJnotify {
+
+
+    public static void co() {
+          List<JnotifyCtx> s = new ArrayList<>();
+
+        final Map<String, JnotifyCtx> collect = s.stream().collect(Collectors.groupingBy(JnotifyCtx::toString, Collector.of(
+                JnotifyCtx::new, (t1, t2) -> {
+                }, (t1, t2) -> t1, a -> a, Collector.Characteristics.IDENTITY_FINISH
+        )));
+    }
 }
