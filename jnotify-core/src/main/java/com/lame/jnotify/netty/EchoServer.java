d0048852d59b1a6c9f85149cb78f3bce76d3610e
guoxj <guoxj@koal.com>
2021-03-23 18:42:35 +0800

    refactor: jnotify重构
    重构支持多git->多目录

---
+++ jnotify-core/src/main/java/com/lame/jnotify/netty/EchoServer.java
@@ -0,0 +1,56 @@
+package com.lame.jnotify.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+public class EchoServer {
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
+
+    public static void main(String[]args) throws Exception {
+        final SslContext sslContext;
+
+        if (SSL) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            sslContext = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
+        }else {
+            sslContext = null;
+        }
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            EchoServerHandler echoServerHandler = new EchoServerHandler();
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            if (sslContext != null) {
+                                p.addLast(sslContext.newHandler(ch.alloc()));
+                            }
+
+                            p.addLast(echoServerHandler);
+                        }
+                    });
+            ChannelFuture f = b.bind(PORT).sync();
+            //等待执行完成关闭
+            f.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ jnotify-core/src/main/java/com/lame/jnotify/netty/EchoServerHandler.java
@@ -0,0 +1,23 @@
+package com.lame.jnotify.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println(msg);
+        ctx.write(msg);
+    }
+}
